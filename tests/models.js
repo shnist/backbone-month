58def49a715fce395adcf651bfa03b16161c1dfa
Nick Collings <nick@collingo.com>
2013-02-04 18:28:56 +0000

    moved libs to libs, added models test runnner

---
+++ tests/models.js
@@ -0,0 +1,8 @@
+describe("The Todo class", function() {
+  it("exists", function() {
+    expect(Todo).toBeDefined();
+  });
+  it("is a function", function() {
+  	expect(typeof Todo).toBe("function");
+  });
+});
