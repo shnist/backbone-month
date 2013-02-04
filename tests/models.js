45dc20797f99abf7d0aacd665d1f5ea60985cbc4
Nick Collings <nick@collingo.com>
2013-02-04 18:35:01 +0000

    added Backbone model test

--- tests/models.js
+++ tests/models.js
@@ -1,8 +1,17 @@
 describe("The Todo class", function() {
+
   it("exists", function() {
     expect(Todo).toBeDefined();
   });
+
   it("is a function", function() {
   	expect(typeof Todo).toBe("function");
   });
+  
+  it("is a Backbone Model", function() {
+  	expect((new Todo()).cid).toBeDefined();
+  	expect((new Todo()).attributes).toBeDefined();
+  	expect((new Todo()).idAttribute).toBeDefined();
+  });
+
 });
