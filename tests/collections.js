845ab58c32c123b53b26e48415a80f6cac4af23b
Jamie Newman <jamie.newman@gmail.com>
2013-02-05 10:43:43 +0000

    Added collections exercises

--- tests/collections.js
+++ tests/collections.js
@@ -1,8 +1,39 @@
 describe("The Todos collection", function() {
-  it("exists", function() {
-    expect(Todos).toBeDefined();
-  });
-  it("is a function", function() {
-  	expect(typeof Todos).toBe("function");
-  });
+    it("exists", function() {
+        expect(Todos).toBeDefined();
+    });
+    it("is a function", function() {  
+        expect(typeof Todos).toBe("function");
+    });
+    it("has a comparator function defined", function() {  
+        expect(typeof Todos.comparator).toBe("function");
+    });
 });
+
+describe("The myTodoList instance", function() {
+    it("exists", function() {  
+        expect(myTodoList).toBeDefined();
+    });
+    it("is an instance of Todos", function() {  
+        expect(myTodoList instanceof Todos).toBeTruthy();
+    });
+    it("contains the models from the 1st lesson", function() {  
+        expect(myTodoList.length).toBeGreaterThan(0);
+    });
+    it("now contains 3 models", function() {  
+        expect(myTodoList.length).toBe(3);
+    });
+});
+
+describe("The completedTodos array", function() {
+    it("exists", function() {  
+        expect(completedTodos).toBeDefined();
+    });
+    it("is an instance of array", function() {  
+        expect(completedTodos instanceof Array).toBeTruthy();
+    });
+    it("contains at least 1 model", function() {  
+        expect(completedTodos.length).toBeGreaterThan(0);
+    });
+});
+
---
+++ week1.js
@@ -0,0 +1,14 @@
+// LESSON 2: COLLECTIONS
+// 1. Extend Backbone.Collection to create a new Collection called Todos
+
+// 2. Define a sort function that automatically sorts the collection by completed state
+
+// 3. Create an instance of Todos called myTodoList
+
+// 4. Add the Todo models you created in Lesson 1 to this Collection
+
+// 5. Add a third Todo to this collection called 
+
+// 6. Filter the collection to exclude completed Todos, and store the output in a new array called completedTodos
+
+// 7. BONUS: Log the text "Updated!" to the console whenever a todo is added, changed or removed from the collection
